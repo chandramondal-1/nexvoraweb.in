b71a72696858c13affdda5bc45197e7a4ca0b8af
chandramondal-1 <suryamondal64656465@gmail.com>
2026-05-12 13:11:46 +0530

    Fix: add loading state and basename to HashRouter for GitHub Pages compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <HashRouter basename="/nexvoraweb.in/">
       <App />
     </HashRouter>
   </StrictMode>,
